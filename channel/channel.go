71a5b6aabae7c091bee8203373c1662cc471f13f
xialeistudio <xialeistudio@gmail.com>
2021-01-02 16:03:22 +0800

    channel ok

--- channel/channel.go
+++ channel/channel.go
@@ -16,3 +16,38 @@ type Channel interface {
 	RemoteAddr() net.Addr
 	Pipeline() Pipeline
 }
+
+type DefaultChannel struct {
+	util.DefaultAttributeMap
+	conn     net.Conn
+	id       channelid.ChannelId
+	pipeline Pipeline
+}
+
+func (c *DefaultChannel) Read(p []byte) (n int, err error) {
+	return c.conn.Read(p)
+}
+
+func (c *DefaultChannel) Write(p []byte) (n int, err error) {
+	return c.conn.Write(p)
+}
+
+func (c *DefaultChannel) Close() error {
+	return c.conn.Close()
+}
+
+func (c *DefaultChannel) Id() channelid.ChannelId {
+	return c.id
+}
+
+func (c *DefaultChannel) LocalAddr() net.Addr {
+	return c.conn.LocalAddr()
+}
+
+func (c *DefaultChannel) RemoteAddr() net.Addr {
+	return c.conn.RemoteAddr()
+}
+
+func (c *DefaultChannel) Pipeline() Pipeline {
+	panic("implement me")
+}
